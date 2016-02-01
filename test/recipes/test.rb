656ba4e322c9ae352fb57c3eaabe469d8735e2ea
Kelly <herberk@uecb1d75d474f565ecdad.ant.amazon.com>
2016-02-01 08:45:37 +0200

    modified recipe test

--- test/recipes/test.rb
+++ test/recipes/test.rb
@@ -1,2 +1,3 @@
 instance = search("aws_opsworks_instance", "self:true").first
 Chef::Log.info("********** For instance '#{instance['instance_id']}', the instance's public IP address is '#{instance['public_ip']}' **********")
+Chef::Log.info("********** For instance '#{instance['instance_id']}', the autoscaling type is '#{instance['auto_scaling_type']}' **********") 
