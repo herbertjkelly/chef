72c633be8924889bab2d5eb3b20b3d2ec9ed40bf
Kelly <herberk@uecb1d75d474f565ecdad.ant.amazon.com>
2016-02-01 09:02:25 +0200

    modified recipe test added conditional statement

--- test/recipes/test.rb
+++ test/recipes/test.rb
@@ -1,6 +1,6 @@
 instance = search("aws_opsworks_instance", "self:true").first
 Chef::Log.info("********** For instance '#{instance['instance_id']}', the instance's public IP address is '#{instance['public_ip']}' **********")
 Chef::Log.info("********** For instance '#{instance['instance_id']}', the autoscaling type is '#{instance['auto_scaling_type']}' **********") I
- if instance['auto_scaling_type'] == null
+ if instance['auto_scaling_type'] == ""
     Chef::Log.info("******* This is a 24/7 instance **********")
   end
