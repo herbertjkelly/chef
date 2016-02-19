19e50cc2082e4537e40ee534d74fc64f1ea98445
Herbert Kelly <herbertkelly@gmail.com>
2016-02-19 12:22:04 +0200

    Update s3get.rb

--- test/recipes/s3get.rb
+++ test/recipes/s3get.rb
@@ -7,6 +7,3 @@ directory "/opt/local" do
   recursive true
   action :create
 end
-
-s3 = Aws::S3::Client.new(region:'us-east-1')
-s3.get_object({bucket: "herberk1", key:"test.zip"},target"/opt/local/test.zip")
