38bca568c2b5773e577436640da6009571bcb5e3
Herbert <herbertkelly@gmail.com>
2016-02-27 14:34:01 +0200

    delted files

--- test/recipes/test.rb
+++
@@ -1,6 +0,0 @@
-instance = search("aws_opsworks_instance", "self:true").first
-Chef::Log.info("********** For instance '#{instance['instance_id']}', the instance's public IP address is '#{instance['public_ip']}' **********")
-Chef::Log.info("********** For instance '#{instance['instance_id']}', the autoscaling type is '#{instance['auto_scaling_type']}' **********")
- if instance['auto_scaling_type'] == nil
-    Chef::Log.info("******* This is a 24/7 instance **********")
- end
