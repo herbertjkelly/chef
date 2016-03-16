507db202470e92b51fd4d58415c65a1c1f22086f
Herbert Kelly <herbertkelly@gmail.com>
2016-03-16 09:00:16 +0200

    Added if statement

--- democook/recipes/echorecipe.rb
+++ democook/recipes/echorecipe.rb
@@ -2,6 +2,10 @@ app = search("aws_opsworks_app").first
 Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
 Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
 
+ if '#{app['shortname']}' == "testapp1"
+    Chef::Log.info("******* I will be deploying as the app matches! **********")
+ end
+
 search("aws_opsworks_app").each do |app|
   Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
   Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
