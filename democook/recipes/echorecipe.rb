29c6741772bcb3ee9bc44e447dd18ba5b1aaf72c
Herbert Kelly <herbertkelly@gmail.com>
2016-03-16 09:15:35 +0200

    Update echorecipe.rb

--- democook/recipes/echorecipe.rb
+++ democook/recipes/echorecipe.rb
@@ -2,7 +2,7 @@ app = search("aws_opsworks_app").first
 Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
 Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
 
- if '#{app['shortname']}' == "testapp1"
+ if {app['shortname']} == "testapp1"
     Chef::Log.info("******* I will be deploying as the app matches! **********")
  end
 
