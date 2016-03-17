84cd258a6634581416963646fb949e2fcc2f356b
Herbert Kelly <herbertkelly@gmail.com>
2016-03-17 10:29:25 +0200

    Update tagebs.rb

--- democook/recipes/tagebs.rb
+++ democook/recipes/tagebs.rb
@@ -1,6 +1,6 @@
 app = search("aws_opsworks_app").first
 Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
-Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
+Chef::Log.info("********** Running BASH**********")
 
 bash 'tagebs' do
   user 'root'
@@ -9,6 +9,6 @@ bash 'tagebs' do
   instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
   region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed "s/[a-zA-Z]$//")
   volumeid=$(aws ec2 describe-instances --instance-ids $instanceid --region $region --query 'Reservations[0].Instances[0].BlockDeviceMappings[0].{VolID:Ebs.VolumeId}' --output text)
-  aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="Root volume of $instanceid" --region $region
+  aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="TEST" --region $region
   EOH
 end
