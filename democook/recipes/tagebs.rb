f9b2ef0b8dddbf6f40bb4333cdf1017819a74712
Herbert Kelly <herbertkelly@gmail.com>
2016-03-17 11:24:15 +0200

    Update tagebs.rb

--- democook/recipes/tagebs.rb
+++ democook/recipes/tagebs.rb
@@ -1,6 +1,8 @@
-app = search("aws_opsworks_app").first
-Chef::Log.info("********** The app's short name is #{app['shortname']} **********")
-Chef::Log.info("********** Running BASH**********")
+stack = search("aws_opsworks_stack").first
+Chef::Log.info("********** The stack's name is '#{stack['name']}' **********")
+
+
+
 
 bash 'tagebs' do
   user 'root'
@@ -9,6 +11,12 @@ bash 'tagebs' do
   instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
   region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed "s/[a-zA-Z]$//")
   volumeid=$(aws ec2 describe-instances --instance-ids $instanceid --region $region --query 'Reservations[0].Instances[0].BlockDeviceMappings[0].{VolID:Ebs.VolumeId}' --output text)
-  aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="TEST - #{app['shortname']}" --region $region
+  aws ec2 create-tags --resources $volumeid --tags Key=StackName,Value="Stackname - #{stack['name']}" --region $region
+  aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="frs-trx-dev.research.i" --region $region
+  aws ec2 create-tags --resources $volumeid --tags Key=Service,Value="frs-research-webservice" --region $region
+  volumeid2=$(aws ec2 describe-instances --instance-ids $instanceid --region $region --query 'Reservations[0].Instances[0].BlockDeviceMappings[1].{VolID:Ebs.VolumeId}' --output text)
+  aws ec2 create-tags --resources $volumeid2 --tags Key=StackName,Value="Stackname - #{stack['name']}" --region $region
+  aws ec2 create-tags --resources $volumeid2 --tags Key=Name,Value="frs-trx-dev.research.i" --region $region
+  aws ec2 create-tags --resources $volumeid2 --tags Key=Service,Value="frs-research-webservice" --region $region
   EOH
 end
