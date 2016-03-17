04cd68aa3a72c7e1ce53fa5f762e5ee153e75599
Herbert Kelly <herbertkelly@gmail.com>
2016-03-17 10:34:37 +0200

    Update tagebs.rb

--- democook/recipes/tagebs.rb
+++ democook/recipes/tagebs.rb
@@ -9,6 +9,6 @@ bash 'tagebs' do
   instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
   region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed "s/[a-zA-Z]$//")
   volumeid=$(aws ec2 describe-instances --instance-ids $instanceid --region $region --query 'Reservations[0].Instances[0].BlockDeviceMappings[0].{VolID:Ebs.VolumeId}' --output text)
-  aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="TEST" --region $region
+  aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="TEST - '#{app['shortname']}'" --region $region
   EOH
 end
