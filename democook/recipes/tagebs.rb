af839d6b5ae84b4fa303224109226133dd2323c2
Herbert Kelly <herbertkelly@gmail.com>
2016-03-17 14:28:22 +0200

    Update tagebs.rb

--- democook/recipes/tagebs.rb
+++ democook/recipes/tagebs.rb
@@ -5,16 +5,20 @@ bash 'tagebs' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
+  #!/bin/bash
   instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
   region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed "s/[a-zA-Z]$//")
-  volumeid=$(aws ec2 describe-instances --instance-ids $instanceid --region $region --query 'Reservations[0].Instances[0].BlockDeviceMappings[0].{VolID:Ebs.VolumeId}' --output text)
-  aws ec2 create-tags --resources $volumeid --tags Key=StackName,Value="Stackname - #{stack['name']}" --region $region
-  aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="frs-trx-dev.research.i" --region $region
-  aws ec2 create-tags --resources $volumeid --tags Key=Service,Value="frs-research-webservice" --region $region
-  volumeid2=$(aws ec2 describe-instances --instance-ids $instanceid --region $region --query 'Reservations[0].Instances[0].BlockDeviceMappings[1].{VolID:Ebs.VolumeId}' --output text)
-  aws ec2 create-tags --resources $volumeid2 --tags Key=StackName,Value="Stackname - #{stack['name']}" --region $region
-  aws ec2 create-tags --resources $volumeid2 --tags Key=Name,Value="frs-trx-dev.research.i" --region $region
-  aws ec2 create-tags --resources $volumeid2 --tags Key=Service,Value="frs-research-webservice" --region $region
+  for i in {0..100}
+    do
+        volumeid=$(aws ec2 describe-instances --instance-ids $instanceid --region $region --query 'Reservations[0].Instances[0].BlockDeviceMappings['$i'].{VolID:Ebs.VolumeId}' --output text)
+        if ! [ $volumeid = "None" ]; then
+                aws ec2 create-tags --resources $volumeid --tags Key=StackName,Value="Stackname - #{stack['name']}" --region $region
+                aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="frs-trx-dev.research.i" --region $region
+                aws ec2 create-tags --resources $volumeid --tags Key=Service,Value="frs-research-webservice" --region $region
+        else
+        break
+        fi
+done
   EOH
 end
 Chef::Log.info("********** Finished Bash to tag EBS volumes **********")
