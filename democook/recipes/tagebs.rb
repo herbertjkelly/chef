601c94057bcafd88c4787d69daffb83c4ac0f5d2
Herbert Kelly <herbertkelly@gmail.com>
2016-03-17 10:51:15 +0200

    Update tagebs.rb

--- democook/recipes/tagebs.rb
+++ democook/recipes/tagebs.rb
@@ -1,5 +1,5 @@
 app = search("aws_opsworks_app").first
-Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+Chef::Log.info("********** The app's short name is #{app['shortname']} **********")
 Chef::Log.info("********** Running BASH**********")
 
 bash 'tagebs' do
@@ -9,7 +9,6 @@ bash 'tagebs' do
   instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
   region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed "s/[a-zA-Z]$//")
   volumeid=$(aws ec2 describe-instances --instance-ids $instanceid --region $region --query 'Reservations[0].Instances[0].BlockDeviceMappings[0].{VolID:Ebs.VolumeId}' --output text)
-  echo app['shortname']
-  #aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="TEST - #{app['shortname']}" --region $region
+  #aws ec2 create-tags --resources $volumeid --tags Key=Name,Value="TEST - app['shortname']" --region $region
   EOH
 end
