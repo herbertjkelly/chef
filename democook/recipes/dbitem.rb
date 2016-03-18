2aa3f44e65e711e4121b10477c7c073bcafbf999
Herbert Kelly <herbertkelly@gmail.com>
2016-03-18 08:33:36 +0200

    Create dbitem.rb

---
+++ democook/recipes/dbitem.rb
@@ -0,0 +1,3 @@
+app = data_bag_item("aws_opsworks_app", "testapp")
+Chef::Log.info("********** The app's short name is '#{app['shortname']}' **********")
+Chef::Log.info("********** The app's URL is '#{app['app_source']['url']}' **********")
