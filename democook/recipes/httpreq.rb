080481d15a5e6a40da75369dc865f30da250f43f
Herbert Kelly <herbertkelly@gmail.com>
2016-03-19 08:49:11 +0200

    Update httpreq.rb

--- democook/recipes/httpreq.rb
+++ democook/recipes/httpreq.rb
@@ -1,5 +1,3 @@
-http_request 'instanceid' do
-  url 'http://169.254.169.254/latest/meta-data/instance-id'
-end
-
+require 'net/http'
+instanceid = Net::HTTP.get(http://169.254.169.254/latest/meta-data/instance-id)
 Chef::Log.info("********** #{instanceid} **********")
