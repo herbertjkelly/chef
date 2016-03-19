7cb28cab6607457ff2070590835f5a48725faaf8
Herbert Kelly <herbertkelly@gmail.com>
2016-03-19 08:40:16 +0200

    Update httpreq.rb

--- democook/recipes/httpreq.rb
+++ democook/recipes/httpreq.rb
@@ -2,4 +2,4 @@ http_request 'instanceid' do
   url 'http://169.254.169.254/latest/meta-data/instance-id'
 end
 
-Chef::Log.info("********** {instanceid} **********")
+Chef::Log.info("********** #{instanceid} **********")
