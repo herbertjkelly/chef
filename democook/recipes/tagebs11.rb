409fad03333a57ad53b867038fc8f629b3065c6f
Herbert Kelly <herbertkelly@gmail.com>
2016-03-19 08:35:46 +0200

    Create tagebs11.rb

---
+++ democook/recipes/tagebs11.rb
@@ -0,0 +1,15 @@
+include_recipe "aws"
+
+unless node['aws-tag']['tags'].empty? || node['aws-tag']['tags'].nil?
+	aws_resource_tag node['ec2']['instance_id'] do
+    	tags(node['aws-tag']['tags'])
+    	action :update
+	end
+
+    Chef::Log.info "****** Tagging ebs ******"
+	aws_resource_tag "db_ebs_volume" do
+      resource_id lazy { node['aws']['ebs_volume']['db_ebs_volume']['volume_id'] }
+      tags(node['aws-tag']['tags'])
+      action :update
+    end
+end
