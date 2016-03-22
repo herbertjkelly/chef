e7800c6bbf7afc0d44013af7c7583a00e4cf8a44
Herbert Kelly <herbertkelly@gmail.com>
2016-03-22 13:40:52 +0200

    Create tag1.rb

---
+++ democook/recipes/tag1.rb
@@ -0,0 +1,24 @@
+chef_gem "aws-sdk" do
+  compile_time true
+  action :install
+end
+gem 'aws-sdk'
+require 'aws-sdk'
+instance = node['ec2']['instance_id']
+toTag = Array.new
+toTag.push(instance)
+ec2 = Aws::EC2::Client.new( :region => "us-east-1")
+instance_attributes = ec2.describe_instance_attribute({
+  instance_id: instance,
+  attribute: "blockDeviceMapping"
+})
+instance_attributes.block_device_mappings.each do|mapping|
+  toTag.push(mapping.ebs.volume_id)
+end
+tag_instance = ec2.create_tags({
+  resources: toTag,
+  tags: node['aws-tag']['tags']
+})
+
+
+
